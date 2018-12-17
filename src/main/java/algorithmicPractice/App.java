e6ce1c6af7e4cc6f7021d0d8557eef4fedb15924
wangyuheng <yuheng.wang@downjoy.com>
2018-12-17 09:06:13 +0800

    init

---
+++ src/main/java/algorithmicPractice/App.java
@@ -0,0 +1,13 @@
+package algorithmicPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
